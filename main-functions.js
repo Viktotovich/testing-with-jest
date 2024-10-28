a813488cdc5415a464da56acb415522a98b3b5bc
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2024-10-28 17:17:56 +0400

    "Feature #2: reverseString tests passed, and function works as per test spec"

--- main-functions.js
+++ main-functions.js
@@ -1,12 +1,20 @@
 const functionController = {
   isWorking: function () {
-    console.log("ffs jest work");
     return true;
   },
   capitalize: function (string) {
     let firstLetter = string.slice(0, 1).toUpperCase();
     return firstLetter + string.slice(1);
   },
+  reverseString: function (string) {
+    let reverseStringArr = [];
+    for (let i = string.length - 1; i >= 0; i--) {
+      reverseStringArr.push(string[i]);
+    }
+
+    return reverseStringArr.join("");
+  },
 };
 
+functionController.reverseString("Vlad");
 export { functionController };
--- main-functions.test.js
+++ main-functions.test.js
@@ -11,3 +11,15 @@ test("Capitalization", () => {
 test("Capitalization pt II", () => {
   expect(functionController.capitalize("non-capital")).toBe("Non-capital");
 });
+
+test("If string is reversed", () => {
+  expect(functionController.reverseString("jest")).toBe("tsej");
+});
+
+test("If string is reversed pt II", () => {
+  expect(
+    functionController.reverseString(
+      "There are 12 chickens in the chicken coup"
+    )
+  ).toBe("puoc nekcihc eht ni snekcihc 21 era erehT");
+});
